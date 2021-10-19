754c9c46d25c0f1c757d05ab16c40a0c9e08494a
Maksym <maksym.vechirko@axiomc.com>
2021-10-19 10:54:47 +0300

    Initial Commit

---
+++ AMATest/AMATest.h
@@ -0,0 +1,18 @@
+//
+//  AMATest.h
+//  AMATest
+//
+//  Created by Maksym on 19/10/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AMATest.
+FOUNDATION_EXPORT double AMATestVersionNumber;
+
+//! Project version string for AMATest.
+FOUNDATION_EXPORT const unsigned char AMATestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AMATest/PublicHeader.h>
+
+
